cedd1fbcba0758a0e2d73659979b171a4604c7af
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-11 19:14:04 -0700

    first version

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//Program that uses the class complexType
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
